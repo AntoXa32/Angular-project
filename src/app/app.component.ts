71512ad6c124eab4618a250b5377a9f8bf34cb22
Anton_Yashchenko <151663096+AntoXa32@users.noreply.github.com>
2025-04-01 20:36:23 +0300

    add new commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,20 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { TestComponent } from './components/test/test.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, TestComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
   title = 'interview-master';
+  messageFromParent = 'Я твій батько';
+
+  messageFromChild = '';
+
+  getMessageFromChild(message: string) {
+    this.messageFromChild = message;
+  }
 }
